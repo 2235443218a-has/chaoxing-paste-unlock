88b5c036c8f23f178ab3981e50c906bfe854c4c1
2235443218a-has <2235443218a@gamil.com>
2025-12-12 12:32:38 +0800

    Update: fix readme and script

--- paste-unlock.user.js
+++ paste-unlock.user.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      1.0
 // @description  你的同学专享版：解除学习通输入框禁止粘贴的限制，让讨论不再手打！
-// @author       你的名字(或者GitHub ID)
+// @author       2235443218a-has
 // @match        *://*.chaoxing.com/*
 // @match        *://*.erya.100.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=chaoxing.com
