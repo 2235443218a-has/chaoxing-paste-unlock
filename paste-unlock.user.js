35805e3bb2bac3d0261d88d2c1b57b5f24b4b61b
2235443218a-has <2235443218a@gamil.com>
2025-12-12 13:07:35 +0800

    Update namespace

--- paste-unlock.user.js
+++ paste-unlock.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         学习通/超星 解除粘贴限制 (Classroom Edition)
-// @namespace    http://tampermonkey.net/
+// @namespace    https://github.com/2235443218a-has
 // @version      1.0
 // @description  你的同学专享版：解除学习通输入框禁止粘贴的限制，让讨论不再手打！
 // @author       2235443218a-has
