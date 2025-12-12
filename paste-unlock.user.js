85fc5bcc7ba89ca1a480d9d891738af52d976bc6
2235443218a-has <2235443218a@gamil.com>
2025-12-12 11:50:48 +0800

    First commit: unlock paste script

---
+++ paste-unlock.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         学习通/超星 解除粘贴限制 (Classroom Edition)
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  你的同学专享版：解除学习通输入框禁止粘贴的限制，让讨论不再手打！
+// @author       你的名字(或者GitHub ID)
+// @match        *://*.chaoxing.com/*
+// @match        *://*.erya.100.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=chaoxing.com
+// @grant        none
+// @run-at       document-end
+// ==/UserScript==
